486b53cad66ff8ba59e90df6a8168c3462144dce
James Lin <jameslin101@gmail.com>
2012-08-17 02:34:08 -0400

    working version

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -13,21 +13,12 @@ class Poll
   field :week,              type: Integer
   field :scoring,           type: String, default: QUESTION_TYPES[0]
   field :note,              type: String
-  field :votes,             type: Integer, default: 0
-  field :max_vote_options,  type: Integer, default: 1
-  field :user_votes         type: Hash, default: {}
+
     
   belongs_to :user
   has_many :poll_options
   accepts_nested_attributes_for :poll_options  
-  attr_accessible :question, :week, :scoring, :note, :votes, :max_vote_options, :user_votes
   
-  def add_vote(user)
-    if self.user_votes.has_key?(user._id)
-      v = self.user_votes[user._id]
-      if v < self.max_vote_options
-        user_votes.store(user._id, v+1)
-    else
-      self.user_votes.
+
   
 end
