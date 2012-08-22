e73b7630c51e488ebc0e7738102cb6414ce08643
James Lin <jameslin101@gmail.com>
2012-08-22 02:52:17 -0400

    attempting fb connect2

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
 
+  accepts_nested_attributes_for :profile
+
   has_many :polls
 
   field :provider,    :type => String
@@ -57,7 +59,7 @@ class User
   end
   
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_create do |user|
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
@@ -65,6 +67,8 @@ class User
       user.last_name = auth.info.last_name
     end
   end
+  
+  
   def self.new_with_session(params, session)
     if session["devise.user_attributes"]
       new(session["devise.user_attributes"], without_protection: true) do |user|
