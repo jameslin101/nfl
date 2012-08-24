8881a0005d91d232508cf9bc63a0f9566657e13b
James Lin <jameslin101@gmail.com>
2012-08-24 18:52:03 -0400

    added kaminari pagination 5

--- app/controllers/player_grids_controller.rb
+++
@@ -1,9 +0,0 @@
-class PlayerGridsController < ApplicationController
-  require 'will_paginate/array'
-
-  def index
-    @player_grid = PlayerGrid.new(params[:player_grid])
-    @assets = @player_grid.assets.paginate(:page => params[:page])
-    
-  end
-end
